ac7e37087ab98991df2100fbad87560cd01c1782
hadi <hadizraika123@gmail.com>
2025-02-19 20:29:16 +0200

    Adding the variable of percentage

--- Finance-Manager.py
+++ Finance-Manager.py
@@ -1,2 +1,6 @@
 salary = float(input("Enter your salary of the month: "))
 month  = input("Enter the month: ")
+
+savings_percent       = float(input("Enter the percentage of salary allocated to savings: "))
+rent_percent          = float(input("Enter the percentage of salary allocated to rent: "))
+electrecity_percent   = float(input("Enter the percentage of salary allocated to electrecity: "))
