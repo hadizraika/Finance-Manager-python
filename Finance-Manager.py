ad72d9c1dc666094c2aa5cde160446da4e55c32d
hadi <hadizraika123@gmail.com>
2025-02-19 21:18:49 +0200

    Adding calculate allocations

--- Finance-Manager.py
+++ Finance-Manager.py
@@ -1,9 +1,14 @@
 salary = float(input("Enter your salary of the month: "))
-month  = input("Enter the month: ")
+month = input("Enter the month: ")
 
-savings_percent       = float(input("Enter the percentage of salary allocated to savings: "))
-rent_percent          = float(input("Enter the percentage of salary allocated to rent: "))
-electrecity_percent   = float(input("Enter the percentage of salary allocated to electrecity: "))
+savings_percent = float(input("Enter the percentage of salary allocated to savings: "))
+rent_percent = float(input("Enter the percentage of salary allocated to rent: "))
+electrecity_percent = float(input("Enter the percentage of salary allocated to electrecity: "))
+
+#calculating
+savings = (savings_percent/100) * salary
+rent = (rent_percent/100) * salary
+electrecity = (electrecity_percent/100) * salary
 
 print(salary)
 print(month)
